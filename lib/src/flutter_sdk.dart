8c52f9d1eab184858c1d7f47ea8e2ffddc3c13d9
Maikel Rehl <maikel@phntm.xyz>
2024-05-27 14:20:45 +0200

    Download puro binary into plugin and use symlinks to puro flutter sdk

---
+++ lib/src/flutter_sdk.dart
@@ -0,0 +1,49 @@
+import 'package:dcli/dcli.dart' as dcli;
+import 'package:puro_sidekick_plugin/puro_sidekick_plugin.dart';
+import 'package:sidekick_core/sidekick_core.dart';
+
+/// Create an empty folder for flutter sdk symlink
+String flutterSdkSymlink() {
+  final flutterPath = Directory("${SidekickContext.sidekickPackage.buildDir.absolute.absolute.path}/flutter");
+  flutterPath.createSync(recursive: true);
+  return flutterPath.absolute.path;
+}
+
+/// Returns the Flutter SDK path from puro environment
+/// throws [PuroNotFoundException] if puro is not found
+String puroFlutterSdkPath() {
+  String? envPath;
+  final pathMatcher = RegExp(r'.*executing: \[(.*)\].*');
+  puro(
+    ['flutter', '-v', '--version'],
+    progress: Progress((line) {
+      if (envPath != null) return;
+      final match = pathMatcher.firstMatch(line);
+      if (match != null) {
+        envPath = match.group(1);
+      }
+    }),
+  );
+  if (envPath == null) {
+    throw PuroNotFoundException();
+  }
+  return envPath!;
+}
+
+/// Create a symlink from link to target
+int createSymlink(String link, String target) {
+  final linkDir = Directory(link);
+  final targetDir = Directory(target);
+  if (linkDir.existsSync()) {
+    linkDir.deleteSync();
+  }
+
+  final process = dcli.startFromArgs(
+    'ln',
+    ['-s', targetDir.path, linkDir.path],
+    nothrow: true,
+    progress: Progress.print(),
+    terminal: true,
+  );
+  return process.exitCode ?? -1;
+}
