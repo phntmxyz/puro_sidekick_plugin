bc3489f75175b32e0dc8fe03633c4c73f227806d
Maikel Rehl <maikel@phntm.xyz>
2024-05-28 15:58:12 +0200

    Add symlink on windows

--- lib/src/flutter_sdk.dart
+++ lib/src/flutter_sdk.dart
@@ -38,12 +38,26 @@ int createSymlink(String link, String target) {
     linkDir.deleteSync();
   }
 
-  final process = dcli.startFromArgs(
-    'ln',
-    ['-s', targetDir.path, linkDir.path],
-    nothrow: true,
-    progress: Progress.print(),
-    terminal: true,
-  );
-  return process.exitCode ?? -1;
+  if (Platform.isMacOS || Platform.isLinux) {
+    final process = dcli.startFromArgs(
+      'ln',
+      ['-s', targetDir.path, linkDir.path],
+      nothrow: true,
+      progress: Progress.print(),
+      terminal: true,
+    );
+    return process.exitCode ?? -1;
+  } else if (Platform.isWindows) {
+    final process = dcli.startFromArgs(
+      'mklink',
+      ['/D', linkDir.path, targetDir.path],
+      nothrow: true,
+      progress: Progress.print(),
+      terminal: true,
+    );
+    return process.exitCode ?? -1;
+  } else {
+    print('Unsupported platform.');
+    return -1;
+  }
 }
