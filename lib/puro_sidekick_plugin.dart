cedaf2ac7671b55fa6f10ef629a8582c1cd3726e
Maikel Rehl <maikel@phntm.xyz>
2024-07-16 17:27:01 +0200

    Update print statement

--- lib/puro_sidekick_plugin.dart
+++ lib/puro_sidekick_plugin.dart
@@ -14,7 +14,8 @@ export 'package:puro_sidekick_plugin/src/flutter_sdk.dart'
 export 'package:puro_sidekick_plugin/src/puro.dart';
 
 void initializePuro(SdkInitializerContext context) {
-  print('Initialize Puro with SDK Context: $context');
+  print(
+      'Initialize Puro with SdkInitializerContext{flutterSdk: ${context.flutterSdk}, dartSdk: ${context.dartSdk}, packageDir: ${context.packageDir}, workingDirectory: ${context.workingDirectory}');
 
   // Create folder for flutter sdk symlink
   final symlinkPath = flutterSdkSymlink();
